c8a72471edbe237c1d3ad20604bc9a4e91b0e666
Oskar <lansoskar@gmail.com>
2023-12-14 15:34:54 +0100

    frontend live test 7

--- src/App.js
+++ src/App.js
@@ -16,15 +16,15 @@ function App() {
                     <nav className="navbar navbar-light bg-light">
                         <ul className='nav-list'>
                             <p className="navbarTitle">Oversigt</p>
-                            <li><a className="nav-link" href="/billabonnement-front.onrender.com">Home </a></li>
-                            <li><a className="nav-link" href="/billabonnement-front.onrender.com/addcar">Opret ny bil </a></li>
-                            <li><a className="nav-link" href="/billabonnement-front.onrender.com/cars">Se alle biler </a></li>
-                            <li><a className="nav-link" href="/-front.onrender.com/makeorder">Opret ny ordre </a></li>
-                            <li><a className="nav-link" href="/billabonnement-front.onrender.com/reservations">Reservationer</a></li>
-                            <li><a className="nav-link" href="/billabonnement-front.onrender.com/addCustomer">Opret kunde </a></li>
-                            <li><a className="nav-link" href="/billabonnement-front.onrender.com/customers">Se alle kunder </a></li>
-                            <li><a className="nav-link" href="/billabonnement-front.onrender.com/createDamageReport">Opret skade-rapport </a></li>
-                            <li><a className="nav-link" href="/billabonnement-front.onrender.com/damageReports">Se alle skade-rapporter </a></li>
+                            <li><a className="nav-link" href="/">Home </a></li>
+                            <li><a className="nav-link" href="/addcar">Opret ny bil </a></li>
+                            <li><a className="nav-link" href="/cars">Se alle biler </a></li>
+                            <li><a className="nav-link" href="/makeorder">Opret ny ordre </a></li>
+                            <li><a className="nav-link" href="/reservations">Reservationer</a></li>
+                            <li><a className="nav-link" href="/addCustomer">Opret kunde </a></li>
+                            <li><a className="nav-link" href="/customers">Se alle kunder </a></li>
+                            <li><a className="nav-link" href="/createDamageReport">Opret skade-rapport </a></li>
+                            <li><a className="nav-link" href="/damageReports">Se alle skade-rapporter </a></li>
                         </ul>
                     </nav>
                 </header>
